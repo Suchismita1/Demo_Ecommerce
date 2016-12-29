3f21ea2c1063549bdcc4661e82adbc62200866bb
Suchismita1 <suchismita.ghadei@gmail.com>
2016-12-29 16:11:17 +0530

    Commit attempt 3 on Day2 changes

---
+++ src/test/java/com/suchi/PageObject/ProductPage.java
@@ -0,0 +1,26 @@
+package com.suchi.PageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ProductPage {
+	String product_detail_price = null;
+
+	public String getProductDetailPrice(WebDriver driver, String productName) {
+		if(productName.equalsIgnoreCase("Sony Xperia") || productName.equalsIgnoreCase("IPhone"))
+		{
+			String product_detail_price_xpath = "//*[@id='product_addtocart_form']//*[contains(text(),'"+productName+"')]//following::span[2]";
+			product_detail_price = driver.findElement(By.xpath(product_detail_price_xpath)).getText();
+			System.out.println("Detail price of the "+productName+" is: "+product_detail_price);
+		}
+		else if(productName.equalsIgnoreCase("Samsung Galaxy"))
+		{
+			String product_detail_price_xpath = "//*[@id='product_addtocart_form']//*[contains(text(),'"+productName+"')]//following::span[5]";
+			product_detail_price = driver.findElement(By.xpath(product_detail_price_xpath)).getText();
+			System.out.println("Detail price of the "+productName+" is: "+product_detail_price);
+		}
+		return product_detail_price;
+		
+	}
+
+}
